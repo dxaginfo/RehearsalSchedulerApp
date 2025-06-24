d4ee1055a94d7a8df808765cb771024da2f50ea7
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-06-24 18:23:55 -0300

    Update models index to use correct config file path

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -5,12 +5,12 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require('../config/database.js')[env];
+const config = require('../config/config.js')[env];
 const db = {};
 
 let sequelize;
-if (config.url) {
-  sequelize = new Sequelize(config.url, config);
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
