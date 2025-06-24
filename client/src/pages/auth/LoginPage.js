31b548b12f2630249b4183dcd743a4408ccf90a2
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-06-24 18:12:50 -0300

    Add login page component

---
+++ client/src/pages/auth/LoginPage.js
@@ -0,0 +1,146 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import {
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  FormControlLabel,
+  Grid,
+  TextField,
+  Typography,
+  Paper,
+  CircularProgress,
+  Alert
+} from '@mui/material';
+
+import { loginUser } from '../../redux/slices/authSlice';
+
+const validationSchema = Yup.object({
+  email: Yup.string()
+    .email('Enter a valid email')
+    .required('Email is required'),
+  password: Yup.string()
+    .min(6, 'Password should be of minimum 6 characters length')
+    .required('Password is required'),
+});
+
+const LoginPage = () => {
+  const dispatch = useDispatch();
+  const { loading, error } = useSelector((state) => state.auth);
+  const [rememberMe, setRememberMe] = useState(false);
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      dispatch(loginUser({ 
+        email: values.email, 
+        password: values.password,
+        rememberMe
+      }));
+    },
+  });
+
+  return (
+    <Container component="main" maxWidth="sm">
+      <Paper elevation={3} sx={{ p: 4, mt: 8 }}>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component="h1" variant="h4" sx={{ mb: 3 }}>
+            Rehearsal Scheduler
+          </Typography>
+          <Typography component="h2" variant="h5" sx={{ mb: 3 }}>
+            Sign In
+          </Typography>
+          
+          {error && (
+            <Alert severity="error" sx={{ width: '100%', mb: 2 }}>
+              {error}
+            </Alert>
+          )}
+          
+          <Box component="form" onSubmit={formik.handleSubmit} noValidate sx={{ mt: 1, width: '100%' }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              error={formik.touched.email && Boolean(formik.errors.email)}
+              helperText={formik.touched.email && formik.errors.email}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              error={formik.touched.password && Boolean(formik.errors.password)}
+              helperText={formik.touched.password && formik.errors.password}
+            />
+            <FormControlLabel
+              control={
+                <Checkbox 
+                  value="remember" 
+                  color="primary" 
+                  checked={rememberMe}
+                  onChange={(e) => setRememberMe(e.target.checked)}
+                />
+              }
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2, py: 1 }}
+              disabled={loading}
+            >
+              {loading ? <CircularProgress size={24} /> : 'Sign In'}
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link to="/forgot-password" style={{ textDecoration: 'none' }}>
+                  <Typography variant="body2" color="primary">
+                    Forgot password?
+                  </Typography>
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link to="/register" style={{ textDecoration: 'none' }}>
+                  <Typography variant="body2" color="primary">
+                    {"Don't have an account? Sign Up"}
+                  </Typography>
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Paper>
+    </Container>
+  );
+};
+
+export default LoginPage;
